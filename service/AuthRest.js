9f9da652656953e02ef63addfbb92c93c9b2ce5d
Vasilii Ermilov <inkz@xakep.ru>
2020-04-28 19:46:16 +1000

    IONEXAMPLE-240 перенос методов апи в один файл

--- service/AuthRest.js
+++ service/AuthRest.js
@@ -27,12 +27,69 @@ function AuthRest(options) {
 
   this._route = function(router) {
 
+    this.addHandler(router, '/', 'GET', (req) => {
+      const {profile} = req.session;
+      if (!profile) {
+        return Promise.reject(new IonError(401, 'Unauthorized'));
+      }
+      return Promise.resolve({data: {profile}});
+    });
+
+    this.addHandler(router, '/', 'POST', (req) => {
+      const {authToken} = req.session;
+      if (!authToken) {
+        return Promise.reject(new IonError(401, 'Unauthorized'));
+      }
+      const {data} = req.body;
+      if (!data) {
+        return Promise.reject(new IonError(400, 'Wrong request'));
+      }
+      return options.backendApi.check(data)
+        .then(() => options.backendApi.apply(authToken, data))
+        .then(() => {
+          req.session.profile = Object.assign(req.session.profile, data);
+          return {data: {profile: req.session.profile}};
+        })
+        .catch((err) => {
+          options.log.error(err);
+          throw new IonError(400, err.message);
+        });
+    });
+
+    function fetchPassports(token, cacheKey) {
+      options.backendApi.passports(token)
+        .then((passports) => {
+          return options.cache.set(cacheKey, passports);
+        })
+        .then(() => passports);
+    }
+
+    this.addHandler(router, '/passports', 'GET', (req) => {
+      const {authToken} = req.session;
+      if (!authToken) {
+        return Promise.reject(new IonError(401, 'Unauthorized'));
+      }
+      const cacheKey = `passports_${req.session.id}`;
+      return options.cache.get(cacheKey)
+        .then((cachedPassports) => {
+          if (cachedPassports) {
+            return cachedPassports;
+          }
+          return fetchPassports(authToken, cacheKey);
+        })
+        .then((passports) => ({data: {passports}}))
+        .catch((err) => {
+          options.log.error(err);
+          throw new IonError(401, err.message);
+        });
+    });
+
     this.addHandler(router, '/login', 'POST', (req) => {
       const {login, password} = req.body;
       if (!login || !password) {
         return Promise.reject(new IonError(400, 'Wrong request'));
       }
-      authenticate(login, password)
+      return authenticate(login, password)
         .then(({token, profile}) => {
           req.session.authToken = token;
           req.session.profile = profile;
@@ -51,6 +108,7 @@ function AuthRest(options) {
       }
       const code = generateSecretCode();
       req.session.registrationCode = code;
+      console.log(code);
       return options.sms.send(phone, `Code: ${code}`)
         .then(() => true)
         .catch((err) => {
